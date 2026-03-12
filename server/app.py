7c0120c66e739be2de8614a7befc6b6453636d0c
+ <jacqueline.trapp18@gmail.com>
2026-03-12 07:27:38 -0400

    fixing app.py to connect BE and FE

--- server/app.py
+++ server/app.py
@@ -9,11 +9,16 @@ from lib.api import ROUTE_MODULES
 # Enable CORS for all routes
 CORS(app)
 
+# Register all API routes on import so they are available under Gunicorn (app:app)
+for register in ROUTE_MODULES:
+    register(api)
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+
 if __name__ == '__main__':
-    for register in ROUTE_MODULES:
-        register(api)
+    # Development server (not used in Docker/Railway)
     app.run(port=5555, debug=True)
